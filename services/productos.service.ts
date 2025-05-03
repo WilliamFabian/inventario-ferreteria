03217ecfabc27c3abd0f164446adf344b5da2713
williamFabian <fabiandel541@gmail.com>
2025-05-03 15:25:10 -0500

    Arreglando servicio

--- services/productos.service.ts
+++ services/productos.service.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
 })
 export class ProductosService {
   //URL del backend.
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:3000/api';
   private productoCreadoSubject = new Subject<void>();
   private selectedOptionSource = new BehaviorSubject<string>('');
   selectedOption$ = this.selectedOptionSource.asObservable(); 
