79155adeb06f2a609274e35af6a6276096c53f81
williamFabian <fabiandel541@gmail.com>
2025-05-06 16:00:33 -0500

    Agregar Registro Quitar fecha SERVER 3

--- backend/server.js
+++ backend/server.js
@@ -102,7 +102,7 @@ apiRouter.post('/:tabla/agregar', validarTabla, (req, res) => {
 
 apiRouter.put("/:tabla/editar", validarTabla, (req, res) => {
   const { tabla } = req.params;
-  const { idVenta, idProducto, idTrabajo, fechaVenta, ...datos } = req.body;
+  const { idVenta, idProducto, idTrabajo, fechaVenta, fechaTrabajo, ...datos } = req.body;
 
   let idCampo;
   let idValor;
