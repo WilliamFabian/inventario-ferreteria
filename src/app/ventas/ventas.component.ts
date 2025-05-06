e02e8c59a2c295c2cd19328c18105cc9384a9268
williamFabian <fabiandel541@gmail.com>
2025-05-06 12:01:51 -0500

    Editar Registro Servicio Cambio de Metodos Fecha Manual 2

--- src/app/ventas/ventas.component.ts
+++ src/app/ventas/ventas.component.ts
@@ -40,6 +40,7 @@ export class VentasComponent {
       valorUnitario: [{ value: '', disabled: true }],
       precioTotal: [{ value: '', disabled: true }],
       descuento: [false],
+      fechaVenta: [''],
     });
   }
 
