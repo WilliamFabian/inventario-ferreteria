ffa2b2fef7a95c3b4e53923df92934cbcc98b69b
williamFabian <fabiandel541@gmail.com>
2025-05-08 19:16:25 -0500

    Arreglando Daño 4

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -75,7 +75,7 @@ export class ProductosComponent {
       nombre: ['', Validators.required],
       tipo: ['', Validators.required],
       precio: ['', [Validators.required, Validators.min(1)]],
-      precioDescuento: [0],
+      precioDescuento: ['0'],
       cantidad: ['', [Validators.required, Validators.min(1)]],
     });
 
