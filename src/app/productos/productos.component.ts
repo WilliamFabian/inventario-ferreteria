da49dbe0ff03377057edc7ad2ac0fbdf049e57c9
williamFabian <fabiandel541@gmail.com>
2025-05-08 20:23:57 -0500

    Arreglando Cantidad Final Final

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -75,7 +75,7 @@ export class ProductosComponent {
       tipo: ['', Validators.required],
       precio: ['', [Validators.required, Validators.min(1)]],
       precioDescuento: [null],
-      cantidad: [null, Validators.min(1)],
+      cantidad: ['', Validators.required],
     });
 
     // Formulario para ventas
